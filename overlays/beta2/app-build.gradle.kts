9100026b3b10eeea13834769bdcdd6bd1c501e97
dogg7776-ops <dogg7776@gmail.com>
2026-09-16 16:32:54 +0800

    Bump K90 Shizuku compatibility build to beta5

--- overlays/beta2/app-build.gradle.kts
+++ overlays/beta2/app-build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.oai.perfpilot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.0.0-beta4-k90-runtimefix"
+        versionCode = 14
+        versionName = "1.0.0-beta5-k90-shizukucompat"
     }
 
     buildTypes {
